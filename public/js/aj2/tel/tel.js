e134a19fb93264686f2a471cd30e62027f3aef2b
sam <admin@heibaidian.com>
2016-04-16 14:03:44 +0800

    Update tel.js

--- public/js/aj2/tel/tel.js
+++ public/js/aj2/tel/tel.js
@@ -11,25 +11,25 @@ angular.module('myAppList2.tel', ['ngRoute'])
 		}
 	])
 
-.controller('tel2Ctrl', ['$scope', '$http', '$templateCache','$routeParams',
-	function($scope, $http, $templateCache,$routeParams) {
-		var url='';
-		if(typeof($routeParams.start)== 'undefined' || typeof($routeParams.pagesize)== 'undefined')
-			url='tel/find/limit/18/0';
-		else
-			url='tel/find/limit/'+$routeParams.pagesize+'/'+$routeParams.start;
+.controller('tel2Ctrl', ['$scope', '$http', '$templateCache','$routeParams','$filter',
+	function($scope, $http, $templateCache,$routeParams,$filter) {
+		// var url='';
+		// if(typeof($routeParams.start)== 'undefined' || typeof($routeParams.pagesize)== 'undefined')
+		// 	url='tel/find/limit/18/0';
+		// else
+		// 	url='tel/find/limit/'+$routeParams.pagesize+'/'+$routeParams.start;
 
-		$http({
-			method: 'GET',
-			url: url,
-			cache: $templateCache
-		}).
-		success(function(data, status) {
-			$scope.list = data;
-		}).
-		error(function(data, status) {
-			$scope.list = data || "Request failed";
-		});
+		// $http({
+		// 	method: 'GET',
+		// 	url: url,
+		// 	cache: $templateCache
+		// }).
+		// success(function(data, status) {
+		// 	$scope.list = data;
+		// }).
+		// error(function(data, status) {
+		// 	$scope.list = data || "Request failed";
+		// });
 
 		$http({
 			method: 'GET',
@@ -55,5 +55,47 @@ angular.module('myAppList2.tel', ['ngRoute'])
 		error(function(data, status) {
 			$scope.sort2 = data || "Request failed";
 		});
+
+		var vm=this;
+		vm.figureOutItemsToDisplay=figureOutItemsToDisplay;
+		
+	    vm.pagedItems = [];
+	    vm.itemsPerPage = 18;
+	    $scope.currentPage = vm.currentPage = 1;
+
+	    function figureOutItemsToDisplay() {
+	    	$http({
+				method: 'GET',
+				url: 'tel/find'
+			}).
+			success(function(data) {
+				vm.filteredItems = $filter('filter')(data, {area:$scope.q2,cate:$scope.q});
+				
+				$scope.arr=[];
+				var len=vm.filteredItems.length%vm.itemsPerPage>0?parseInt(vm.filteredItems.length/vm.itemsPerPage)+1:parseInt(vm.filteredItems.length/vm.itemsPerPage);
+				for (var i = 1; i <=len; i++) 
+					$scope.arr.push(i);
+				
+				vm.filterLength = vm.filteredItems.length;
+		      	var begin = ((vm.currentPage - 1) * vm.itemsPerPage);
+		      	var end = begin + vm.itemsPerPage;
+
+		      	if(vm.filterLength<begin){
+		      		$scope.pagedItems = vm.filteredItems.slice(0, vm.itemsPerPage);
+		      		$scope.currentPage = vm.currentPage = 1;
+		      	}else
+		      		$scope.pagedItems = vm.filteredItems.slice(begin, end);
+			})
+	    }
+
+	    $scope.pageChanged=function() {
+	      vm.figureOutItemsToDisplay();
+	    }
+
+	    $scope.changePage=function(d) {
+	      $scope.currentPage = vm.currentPage = d;
+	    }
+
+	    $scope.$watch('currentPage', vm.figureOutItemsToDisplay);
 	}
 ]);
